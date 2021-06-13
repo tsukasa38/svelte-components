9b067bdeaeee354bbdbba537a338266d845f2739
tsukasa38 <104.tsukasa@gmail.com>
2021-06-13 15:43:35 +0000

    Create Demo Page

---
+++ example/src/components/Button.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+    export let handleClick: () => void;
+</script>
+
+<button on:click={handleClick}>
+    <slot></slot>
+</button>
+
+<style>
+    button {
+        margin: 3px;
+        padding: 12px;
+        border: none;
+        outline: none;
+        color: white;
+        cursor: pointer;
+        font-size: 1rem;
+        line-height: 1rem;
+        border-radius: 4px;
+        background-color: dodgerblue;
+        box-shadow: 1px 1px 1px 1px #dddddd;
+    }
+    button:active {
+        box-shadow: none;
+    }
+</style>
